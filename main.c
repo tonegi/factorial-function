e050df766ac53f96e5ce4dcb974db317dd73a0ea
tonegi <90652377+tonegi@users.noreply.github.com>
2024-10-08 12:14:01 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int factorial(int n);
+
+int main(void) {
+ int n = 0;
+  printf("Enter a whole number: ");
+    scanf("%d", &n);
+  printf("%d", factorial(n));
+  return 0;
+}
+
+int factorial(int n) {
+  return n == 1 || n == 0 ? 1 : (n * fatorial(n - 1));
+}
